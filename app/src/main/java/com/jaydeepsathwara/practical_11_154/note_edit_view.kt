1e9b72526dc50fa756b1f729f5f6a27ca62fc661
jaydeep <jaydeepsathwara272@gmail.com>
2022-11-20 23:20:52 +0530

    Initial commit

---
+++ app/src/main/java/com/jaydeepsathwara/practical_11_154/note_edit_view.kt
@@ -0,0 +1,11 @@
+package com.jaydeepsathwara.practical_11_154
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class note_edit_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.note_edit_view)
+    }
+}
